d1a1bc6e5fb453b54231f0f37f2cf8e15ec0f2eb
marta nizynska <78944173+martaniz@users.noreply.github.com>
2021-08-05 19:44:13 -0400

    final

--- work scheduler/script.js
+++ work scheduler/script.js
@@ -1,4 +1,47 @@
-var todaysDate = ""; 
-var todaysDateString = ""; 
-var whatHour = 9; 
-var time = []; 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+
+
+function timeBlocks () {
+    var i = 0,
+        blocks = [],
+        b = {};
+    
+    for (i; i < 48; i++) {
+        b = block(i / 2);
+        blocks.push({block: i + 1, military: b.mil, standard: b.std});
+    }
+    
+    return blocks;
+}
+
+function block(t) {
+    var m = t,
+        meridian = t >= 12 ? "pm" : "am",
+        s = !Math.floor(t) % 12 ? (t % 12) + 12 : t % 12;
+    
+    function convert (time) {
+        var hrs = Math.floor(time),
+            mins = (time - hrs) * 60;
+            
+        return {
+            std: `${hrs}:${mins < 10 ? "0" + mins : mins} ${meridian}`,
+            mil: `${hrs < 10 ? "0" + hrs : hrs}:${mins < 10 ? "0" + mins : mins}`
+        };
+    }
+    
+    return {
+        std: convert(s).std,
+       
+// run this program! mil: convert(m).mil
+    };
+}
+
+var b = timeBlocks();
+console.log(b);
